17de6144789d8c9ff451e0e6ce72873343c4aef4
github username <github email address>
2020-01-22 14:52:27 -0700

    fixed runner and cli files added seed.db

--- runner.rb
+++ runner.rb
@@ -1,14 +1,14 @@
 require_relative './config/environment'
-puts "Hello! Welcome to Grocery-Bot!"
-puts "What's your name?"
+puts "Welcome to Grocery-Bot. Grocery-Bot allows you to add groceries to your grocery-list"
+puts "Lets get started, may I have your name?"
 
 user_name = gets.chomp
 
-new_user = User.new(user_name)
+user_new = User.new(user_name)
 
-cli = Cli.new(new_user)
 
+cli = Cli.new(user_new)
 cli.welcome_menu
 
-binding.pry
+# binding.pry
 0
