4bb3edb9157a4ff6c0a200be47dacede4a0c645b
Zprogrammer-code <skatboyza124@hotmail.com>
2020-01-22 14:11:35 -0700

    pull request and merge practice

--- lib/models/Cli.rb
+++ lib/models/Cli.rb
@@ -4,6 +4,11 @@ class Cli
         @user = user
     end
 
+    def Start
+        puts "Welcome to Grocery-Bot. Grocery-Bot allows you to add groceries to your grocery-list"
+        puts "Lets get started, may I have your name?"
+        
+    end
     def welcome_menu
         puts "Welcome #{user.name}"
         puts "Would you like to build a new grocery list?"
