a40e4c97e55cac6df6c531b342b6a54dfef352ba
Zprogrammer-code <skatboyza124@hotmail.com>
2020-01-23 09:40:14 -0700

    Just refractered the welcome message method and I call list_creation within that method.

--- lib/models/Cli.rb
+++ lib/models/Cli.rb
@@ -14,25 +14,27 @@ class Cli
         item_objects
     end
 
+
     def welcome_menu
         puts "Welcome #{user.name}"
+        list_creation
+    end
+        
+        
+    def list_creation
         puts "Would you like to build a new grocery list? \n Please enter Yes or No"
         answer = gets.chomp
-
         if answer == "yes" 
-            # show them the list of items
             puts "What is the name of the grocery list?"
             list_name = gets.chomp
             list1 = List.create(name: list_name)
             array = Item.all.map {|item| item.name}
             prompt = TTY::Prompt.new
-            
             choices = prompt.multi_select("Please choose from list of popular household-grocery items below: \n", array)
             grocery_objects = get_item_object_from_choices (choices)
-            
             grocery_objects.map {|choice| List_item.create(list: list1, item: choice)}
 
-            binding.pry
+        
         end
     end
 end
